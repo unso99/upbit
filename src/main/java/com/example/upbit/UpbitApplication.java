a14f2b362d130f3b88303f216e49a4f6e6c2ec9f
unso99 <unso99@naver.com>
2024-10-16 20:08:49 +0900

    first commit

---
+++ src/main/java/com/example/upbit/UpbitApplication.java
@@ -0,0 +1,13 @@
+package com.example.upbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/upbit/UpbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.upbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
