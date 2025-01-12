31f84bd58f32eaa42aed8aef531165bf93535cd2
unso99 <unso99@naver.com>
2025-01-12 20:06:16 +0900

    feat: coin chart

--- src/main/java/com/example/upbit/controller/ChartController.java
+++ src/main/java/com/example/upbit/controller/ChartController.java
@@ -2,14 +2,20 @@ package com.example.upbit.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class ChartController {
 
   @GetMapping("/chart")
-  public String chartInfo(){
+  public String chartInfo(
+      @RequestParam(name = "ticker", required = false, defaultValue = "KRW-BTC") String ticker,
+      Model model) {
+
+    model.addAttribute("ticker", ticker);
     return "chart";
   }
 
